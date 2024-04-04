1b07b914476341c57cb9ee23a7e059ef339e9209
stefanelli990 <stefan.mohenski@gmail.com>
2024-04-04 02:52:23 +0200

    revert playfair display

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,13 +14,13 @@ export default function Navbar() {
     return (
         <header className="fixed top-0 left-0 right-0 z-10 bg-white">
             <div className="section-container py-5 flex items-center justify-between text-black">
-                <Link to='/' className="font-serif text-base lg:text-xl font-bold uppercase">Nemanja Photography</Link>
+                <Link to='/' className="font-serif text-base lg:text-xl font-semibold uppercase">Nemanja Photography</Link>
                 <ul className="hidden text-sm md:flex space-x-6">
                     {navLinks.map((navLink, index) => (
                         <li key={index} className="group relative">
                             <Link to={navLink.url}>{navLink.name}</Link>
-                            {location.pathname === navLink.url && <div className="absolute -bottom-1 bg-black h-[2px] w-4 group-hover:w-4"></div>}
-                            <div className="absolute -bottom-1 bg-black h-[2px] w-0 group-hover:w-4 transition-all duration-300"></div>
+                            {location.pathname === navLink.url && <div className="absolute -bottom-1 bg-black h-[1px] w-5 group-hover:w-5"></div>}
+                            <div className="absolute -bottom-1 bg-black h-[1px] w-0 group-hover:w-5 transition-all duration-300"></div>
                         </li>
                     ))}
                 </ul>
